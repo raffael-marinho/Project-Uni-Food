74008f27a73c40a81236126651bea59c2f347970
raffael-marinho <silvamarinho10@gmail.com>
2025-01-08 05:36:12 -0300

    Implementação inicial do VendedorController e ajustes no fluxo de autenticação

---
+++ Server/controllers/VendedorController.js
@@ -0,0 +1,25 @@
+const Vendedor = require('../models/Vendedor');
+
+class VendedorController {
+    async postVendedor() {
+
+    }
+
+    async getAllVendedor() {
+
+    }
+
+    async getOneVendedor() {
+
+    }
+
+    async updateVendedor() {
+
+    }
+
+    async deleteVendedor() {
+
+    }
+}
+
+module.exports = new VendedorController();
