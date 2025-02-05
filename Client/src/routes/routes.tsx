a0338294201536e956bdefa5d0302d839b672774
pauloJ-r <pj74586@gmail.com>
2025-02-05 16:14:47 -0300

    style: alterações na rota

--- Client/src/routes/routes.tsx
+++ Client/src/routes/routes.tsx
@@ -1,12 +1,14 @@
 import {Route, Routes} from 'react-router-dom'
 import Intro from '../sections/intro/index'
 import Home from '../sections/homepage/index'
+import IntroScreen from '../sections/intro/intro'
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Intro />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/intro" element={<IntroScreen />} />
         </Routes>
     )
 }
--- Client/src/sections/intro/index.tsx
+++ Client/src/sections/intro/index.tsx
@@ -1,18 +1,20 @@
 import {uni} from '../../assets/imagens.ts'
 import { Button } from '../../components/ui/button.tsx'
+import { useNavigate } from 'react-router-dom';
 function Intro() {
+  const navigate = useNavigate();
 
   return (
     <>
-      <div className='center flex flex-col p-4 justify-center items-center'>
+      <div className='flex flex-col items-center justify-center h-screen'>
 
-        <img className='w-45 h-45 pt-16 mt-8' src={uni} alt="" />
+        <img className='w-45 h-45' src={uni} alt="" />
         <div className='flex flex-col items-center text-center gap-4 pt-8 m-4'>
         <h1 className='text-4xl font-'>Bem Vindo ao UniFood</h1>
-        <p className='text-lg text-justify mt-4 '>Facilitando sua vida no campus: compre e venda lanches com facilidade.</p>
+        <p className='text-lg text-justify mt-4'>Facilitando sua vida no campus: compre e venda lanches com facilidade.</p>
         </div>
         <div className='flex flex-col items-center text-center pt-10'>
-          <Button className=' w-60'>Vamos Começar</Button>
+          <Button className='w-60' onClick={() => navigate('/intro')}>Vamos Começar</Button>
         </div>
       </div>
     </>
