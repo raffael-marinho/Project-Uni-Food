31b88c8722f24f79f7bd9534a0560ccf897200ef
PdrVinicius <pedro.lopesxavier@gmail.com>
2025-02-05 17:17:18 -0300

    style:estilizando a pág homepage

--- Client/src/sections/homepage/index.tsx
+++ Client/src/sections/homepage/index.tsx
@@ -1,8 +1,11 @@
 function Homepage() {
 
     return (
-        <div>
-            <h1>Homepage</h1>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-semibold">Homepage</h1>
+            <div className="h-13">
+
+            </div>
         </div>
     )
 }
