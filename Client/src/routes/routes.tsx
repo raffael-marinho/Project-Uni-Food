447d59b80c16081bcc0da8659f3f2789b73ea112
pauloJ-r <pj74586@gmail.com>
2025-02-06 11:43:42 -0300

    feat: colocando as rotas para o login de cliente e vendedor

--- Client/src/routes/routes.tsx
+++ Client/src/routes/routes.tsx
@@ -3,6 +3,9 @@ import Intro from '../sections/intro/index'
 import Home from '../sections/homepage/index'
 import IntroScreen from '../sections/intro/intro'
 import Selector from '../sections/selector/index'
+import LoginClient from '../sections/login/loginclient'
+import LoginVendedor from '../sections/login/loginvendedor'
+
 export const AppRoutes = () => {
     return (
         <Routes>
@@ -10,6 +13,8 @@ export const AppRoutes = () => {
             <Route path="/home" element={<Home />} />
             <Route path="/intro" element={<IntroScreen />} />
             <Route path="/selector" element={<Selector />} />
+            <Route path="/logincliente" element={<LoginClient />} />
+            <Route path="/loginvenda" element={<LoginVendedor />} />
         </Routes>
     )
 }
