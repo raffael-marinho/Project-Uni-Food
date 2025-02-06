090f9cbfa43685247d8bd7f63b9afeb394622fd1
pauloJ-r <pj74586@gmail.com>
2025-02-06 00:35:28 -0300

    feat: configurando rota de selector

--- Client/src/routes/routes.tsx
+++ Client/src/routes/routes.tsx
@@ -2,13 +2,14 @@ import {Route, Routes} from 'react-router-dom'
 import Intro from '../sections/intro/index'
 import Home from '../sections/homepage/index'
 import IntroScreen from '../sections/intro/intro'
-
+import Selector from '../sections/selector/index'
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Intro />} />
             <Route path="/home" element={<Home />} />
             <Route path="/intro" element={<IntroScreen />} />
+            <Route path="/selector" element={<Selector />} />
         </Routes>
     )
 }
