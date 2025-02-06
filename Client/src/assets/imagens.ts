9bff82d8aa267dc08b7ebd35598fe198707d7de1
Alyson-Vinicius <149380852+Alyson-Vinicius@users.noreply.github.com>
2025-02-06 18:47:10 -0300

    Style: Adição dos icons

--- Client/src/assets/imagens.ts
+++ Client/src/assets/imagens.ts
@@ -4,11 +4,26 @@ import data from '../assets/imagens/data.svg'
 import lanche from '../assets/imagens/lanche.svg'
 import client from '../assets/imagens/client.svg'
 import chef from '../assets/imagens/chef.svg'
+import usuario from "../assets/imagens/usuario.svg";
+import shoppingcart from "../assets/imagens/shoppingcart.svg";
+import menu from "../assets/imagens/menu.svg";
+import logoMobile from "../assets/imagens/LogoMobile.svg";
+import lupa from "../assets/imagens/lupa.svg";
+import home from "../assets/imagens/home.svg";
+import calendar from "../assets/imagens/calendar.svg";
+
 
 export { uni,
          mercado,
          data,
          lanche,
          client,
-         chef
+         chef,
+         usuario,
+         shoppingcart,
+         menu,
+         logoMobile,
+         lupa,
+         home,
+         calendar
 }
