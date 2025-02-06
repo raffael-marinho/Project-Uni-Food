83599851dc567ad222c541acfeebc9180b952ac8
pauloJ-r <pj74586@gmail.com>
2025-02-06 00:35:02 -0300

    style: adicionando imagens

--- Client/src/assets/imagens.ts
+++ Client/src/assets/imagens.ts
@@ -2,9 +2,13 @@ import uni from '../assets/imagens/uni.svg'
 import mercado from '../assets/imagens/mercado.svg'
 import data from '../assets/imagens/data.svg'
 import lanche from '../assets/imagens/lanche.svg'
+import client from '../assets/imagens/client.svg'
+import chef from '../assets/imagens/chef.svg'
 
 export { uni,
          mercado,
          data,
-         lanche
+         lanche,
+         client,
+         chef
 }
