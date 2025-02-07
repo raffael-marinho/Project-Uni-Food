c1067218f85918cf3b9d35ebbab1a9dc41e7cf99
Alyson-Vinicius <149380852+Alyson-Vinicius@users.noreply.github.com>
2025-02-06 21:05:09 -0300

    feat:componentes Nav na home

--- Client/src/sections/homepage/index.tsx
+++ Client/src/sections/homepage/index.tsx
@@ -1,11 +1,17 @@
+import  Navbar  from "@/components/Navbar";
+import NavInferior from "@/components/Navinferior"; 
+
+
 function Homepage() {
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-semibold">Homepage</h1>
-            <div className="h-13">
+        <div className="flex flex-col  h-screen overflow-x-hidden">
+            <   Navbar />
 
-            </div>
+            <h1 className="pt-16 justify-center items-center text-4xl font-semibold">Homepage</h1>
+            <div className="h-13"> </div> 
+
+            <NavInferior />
         </div>
     )
 }
