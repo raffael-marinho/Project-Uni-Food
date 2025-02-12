c0ce8f8e251537806c7257eb8d5415b928506f85
pauloJ-r <pj74586@gmail.com>
2025-02-12 01:50:26 -0300

    feat: adicionando os provider de auth, nav e toast

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ToastProvider } from "@/components/ui/toast"; // Importe o ToastProvider
+import { Toaster } from './components/ui/toaster';
+import { AuthProvider } from './context/auth-context';
+import { NavProvider } from './context/nav-context';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+    <NavProvider>
+    <ToastProvider> 
+      <App />
+      <Toaster /> 
+    </ToastProvider>
+    </NavProvider>
+    </AuthProvider>
+  </StrictMode>
+);
