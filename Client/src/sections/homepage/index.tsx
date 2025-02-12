15d1fc693882315f8a7766889996b3141bd70e93
pauloJ-r <pj74586@gmail.com>
2025-02-12 01:57:35 -0300

    feat: adicionando na pagina home

--- Client/src/sections/homepage/index.tsx
+++ Client/src/sections/homepage/index.tsx
@@ -1,12 +1,33 @@
-function Homepage() {
+import Navbar from '@/components/Navbar';
+import NavInferior from '@/components/Navinferior';
+import { useEffect, useState } from 'react';
+import { useNav } from '@/context/nav-context';
 
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-semibold">Homepage</h1>
-            <div className="h-13">
+const Home = () => {
+  const [user, setUser] = useState<{ nome: string; telefone: string } | null>(null);
 
-            </div>
-        </div>
-    )
-}
-export default Homepage;
+  const { setActiveTab } = useNav();
+
+  useEffect(() => {
+    // Obtendo o usuário do localStorage
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+
+    // Definindo a aba ativa como "inicio"
+    setActiveTab("inicio");
+  }, []);
+
+  return (
+    <div className="home-container">
+     <Navbar/>
+      <h1>Bem-vindo, {user?.nome || "usuário"}!</h1>
+      <p>Seu Numero é : {user?.telefone || "cidade desconhecida"}</p>
+    <NavInferior/>
+    </div>
+
+  );
+};
+
+export default Home;
