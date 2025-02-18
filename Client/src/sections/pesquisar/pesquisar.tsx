3401516be3db26a9ad21fb3fc175969a52a8e933
pauloJ-r <pj74586@gmail.com>
2025-02-18 18:32:21 -0300

    feat: adicionando pesquisar

---
+++ Client/src/sections/pesquisar/pesquisar.tsx
@@ -0,0 +1,31 @@
+import NavBar from "../../components/Navbar"
+import NavInferior from "../../components/Navinferior"
+import { useNav } from "../../context/nav-context"
+import { useEffect, useState } from "react";
+
+const Pesquisar = () => {
+    const { setActiveTab } = useNav();
+    const [user, setUser] = useState<Record<string, any> | null>(null);
+
+    useEffect(() => {
+        // Obtendo o usuário do localStorage
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+          setUser(JSON.parse(storedUser));
+        }
+
+        setActiveTab("pesquisar");
+      }, []);
+      
+    return (
+        <div>
+            <NavBar/>
+            <div className="flex flex-col items-center h-screen p-5">
+                <h1 className="text-3xl font-bold mb-4">Pesquisar</h1>
+            </div>
+            <NavInferior/>
+        </div>
+    )
+}
+
+export default Pesquisar
