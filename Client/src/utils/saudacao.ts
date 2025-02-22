2b0d5bd6979bcf7a45ea76a2dc494853a739bfdd
pauloJ-r <pj74586@gmail.com>
2025-02-22 20:36:51 -0300

    feat: adicionando funcionalidade de saudação

---
+++ Client/src/utils/saudacao.ts
@@ -0,0 +1,8 @@
+    const getSaudacao = () => {
+    const hora = new Date().getHours();
+    if (hora >= 5 && hora < 12) return "Bom dia";
+    if (hora >= 12 && hora < 18) return "Boa tarde";
+    return "Boa noite";
+  };
+
+  export default getSaudacao;
