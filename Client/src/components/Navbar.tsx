9c6c86a7f5ef0bba64dc9d3010e6a6e28eb779b5
pauloJ-r <pj74586@gmail.com>
2025-02-22 19:15:16 -0300

    refactor: removendo commits desnecessarios

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -5,14 +5,9 @@ import Shoppingcart from "@/assets/imagens/shoppingcart.svg";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-background sticky top-0 w-full shadow-md z-50"> {/* Cor de fundo bege claro */}
+    <nav className="bg-background sticky left-0 top-0 w-full shadow-md z-50 pt-5"> {/* Cor de fundo bege claro */}
       <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        {/* Ícone do menu */}
-        {/* <div>
-          <button className="text-2xl gap-2 mx-5 hover:bg-[#EAA869] hover:text-white rounded-full p-2 duration-200">
-            <img src={menu} alt="menu" />
-          </button>
-        </div> */}
+       
 
         {/* Logo */}
         <div className="w-10 h-10 ml-8 flex-shrink-0">
