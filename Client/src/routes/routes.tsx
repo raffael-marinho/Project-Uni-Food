c846f120091f14f7335bea78276130fd8de75866
pauloJ-r <pj74586@gmail.com>
2025-02-22 20:33:46 -0300

    feat: modificando as rotas com o id

--- Client/src/routes/routes.tsx
+++ Client/src/routes/routes.tsx
@@ -8,6 +8,7 @@ import LoginVendedor from '../sections/login/loginvendedor'
 import Perfil from '../sections/perfil/perfil'
 import Reservas from '../sections/reservas/reservas'
 import Pesquisar from '@/sections/pesquisar/pesquisar'
+import EditarPerfil from '../sections/perfil/editarperfil'
 
 
 export const AppRoutes = () => {
@@ -19,9 +20,10 @@ export const AppRoutes = () => {
             <Route path="/selector" element={<Selector />} />
             <Route path="/logincliente" element={<LoginClient />} />
             <Route path="/loginvenda" element={<LoginVendedor />} />
-            <Route path="/perfil" element={<Perfil />} />
+            <Route path="/perfil/:id" element={<Perfil />} />
             <Route path="/reservas" element={<Reservas />} />
             <Route path="/pesquisar" element={<Pesquisar />} />
+            <Route path="/editarperfil/:id" element={<EditarPerfil />} />
         </Routes>
     )
 }
