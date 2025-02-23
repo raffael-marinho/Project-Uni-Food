da774ae8d778bb1b764c55b247f05aafe408d40e
pauloJ-r <pj74586@gmail.com>
2025-02-23 18:38:51 -0300

    feat: adicionando um padding entre lanches e vendedor

--- Client/src/sections/homepage/index.tsx
+++ Client/src/sections/homepage/index.tsx
@@ -60,7 +60,7 @@ const Home = () => {
            ))}
             
           </div>
-          <div>
+          <div className='pt-4'>
             <h1 className='text-base font-semibold pt-4 pb-2'>Vendedores</h1>
             <div className='flex flex-col gap-4'>
             {dadosVendedor.map((vendedor) => (
