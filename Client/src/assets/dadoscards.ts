8dfd87728265e285d1439faa51eb9842b3a3c0ae
pauloJ-r <pj74586@gmail.com>
2025-02-23 18:25:05 -0300

    feat: adicionando dados para cards de produtos

---
+++ Client/src/assets/dadoscards.ts
@@ -0,0 +1,23 @@
+export const dadosLanches = [
+    {
+        id: 1,
+        titulo: "Hamburguer Artesanal",
+        vendedor: "Lanchonete do João",
+        preco: "19.90",
+        imagem: "https://blog.biglar.com.br/wp-content/uploads/2024/08/iStock-1398630614.jpg"
+    },
+    {
+        id: 2,
+        titulo: "Pizza de Calabresa",
+        vendedor: "Pizzaria da Avenida",
+        preco: "29.90",
+        imagem: "https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3277542:1663012513/Pizza-de-calabresa.jpg?f=default&$p$f=00df3e9"
+    },
+    {
+        id: 3,
+        titulo: "Coxinha",
+        vendedor: "Salgados do João",
+        preco: "5.00",
+        imagem: "https://cdn.casaeculinaria.com/wp-content/uploads/2023/05/16114400/Massa-de-coxinha.webp"
+    }
+];
