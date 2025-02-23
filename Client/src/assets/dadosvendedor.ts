5f1582d49c669869549fa9b338dfb56c57c14930
pauloJ-r <pj74586@gmail.com>
2025-02-23 18:25:18 -0300

    feat: adicionando dados para vendedor

---
+++ Client/src/assets/dadosvendedor.ts
@@ -0,0 +1,51 @@
+export const dadosVendedor = [
+    {
+        id: 1,
+    nome: "João's Burger",
+    status: "Fechado",
+    telefone: "+5581983702609",
+    imagem: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww"
+  },
+  {
+    id: 2,
+    nome: "Carlos Grill",
+    status: "Aberto",
+    telefone: "+5581983702610",
+    imagem: "https://marketplace.canva.com/EAGK6XNcgJM/1/0/1600w/canva-orange-and-yellow-simple-street-food-logo-P8-5uoYzOgo.jpg"
+  },
+  {
+    id: 3,
+    nome: "Maria's Delights",
+    status: "Aberto",
+    telefone: "+5581983702611",
+    imagem: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D"
+  },
+  { 
+    id: 4,
+    nome: "Pedro's Pizza",
+    status: "Fechado",
+    telefone: "+5581983702612",
+    imagem: "https://i.pinimg.com/736x/58/59/e0/5859e005823cfecf3033f6a4a9dbb3f6.jpg"
+  },
+  { 
+    id: 5,
+    nome: "Fernando's Sweets",
+    status: "Aberto",
+    telefone: "+5581983702613",
+    imagem: ""
+  },
+  {
+    id: 6,
+    nome: "Ricardo's BBQ",
+    status: "Fechado",
+    telefone: "+5581983702614",
+    imagem: "https://i.pinimg.com/564x/ca/ff/dc/caffdc755a4396b4c99da3da83e62796.jpg"
+  },
+  {
+    id: 7,
+    nome: "Juliana's Café",
+    status: "Fechado",
+    telefone: "+5581983702615",
+    imagem: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/coffee-logo-design-template-223131a960424d6fdc0db72a044dcb22_screen.jpg?ts=1739798351"
+  },
+]
