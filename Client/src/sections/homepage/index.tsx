04b3803fba208a8529369814077959ffcba90a47
pauloJ-r <pj74586@gmail.com>
2025-02-24 13:24:25 -0300

    feat: adicionando função de loading no inicio e carregar vendedores de 3 em 3

--- Client/src/sections/homepage/index.tsx
+++ Client/src/sections/homepage/index.tsx
@@ -1,6 +1,6 @@
 import Navbar from '@/components/Navbar';
 import NavInferior from '@/components/Navinferior';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNav } from '@/context/nav-context';
 import { banner01, banner02, banner03 } from '@/assets/imagens';
 import getSaudacao from '@/utils/saudacao';
@@ -8,32 +8,65 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/swiper-bundle.css";
 import { useAuth } from '@/context/auth-context';
-import CardLanche from '@/components/cardlanche';
+import CardLanche from '@/components/CardLanche';
 import CardVendedor from '@/components/CardVendedor';
 import { dadosLanches } from '@/assets/dadoscards';
 import { dadosVendedor } from '@/assets/dadosvendedor';
+import Loading from '@/components/Loading';
 
 const Home = () => {
   const { user } = useAuth();
   const { setActiveTab } = useNav();
+  const [loading, setLoading] = useState(true);
+  const [vendedoresExibidos, setVendedoresExibidos] = useState(3); 
+  const [loadingMore, setLoadingMore] = useState(false); 
 
   useEffect(() => {
     setActiveTab("inicio");
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, []);
 
+  // Função para carregar mais vendedores
+  const carregarMaisVendedores = () => {
+    if (loadingMore || vendedoresExibidos >= dadosVendedor.length) return;
+    setLoadingMore(true);
+    setTimeout(() => {
+      setVendedoresExibidos(prevState => prevState + 3); 
+      setLoadingMore(false);
+    }, 1000); 
+  };
+
+  const handleScroll = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+    const bottom = e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.clientHeight;
+    if (bottom) {
+      carregarMaisVendedores();
+    }
+  };
+
   return (
-    <div className='flex flex-col h-screen' >
+    <div className='flex flex-col h-screen'>
       <Navbar />
-      <div className='flex-1 p-4 overflow-y-auto ' style={{ marginBottom: '4rem' }}>
+
+      <div>
+        {loading && (
+          <div className="flex justify-center h-screen pb-64">
+            <Loading />
+          </div>
+        )}
+      </div>
+
+      <div className='flex-1 p-4 overflow-y-auto' style={{ marginBottom: '4rem' }} onScroll={handleScroll}>
         <h1 className='text-xl font-semibold'>{getSaudacao()}, {user?.nome || "usuário"}!</h1>
-        
+
         <div className="mt-2">
           <Swiper
             modules={[Pagination, Autoplay]}
             pagination={{ clickable: true }}
             autoplay={{ delay: 4000, disableOnInteraction: false }}
             loop={true}
-            className="w-full rounded-lg  custom-swiper"
+            className="w-full rounded-lg custom-swiper"
           >
             <SwiperSlide>
               <img src={banner01} alt="Banner 1" className='w-full h-full' />
@@ -42,41 +75,46 @@ const Home = () => {
               <img src={banner02} alt="Banner 2" className='w-full h-full' />
             </SwiperSlide>
             <SwiperSlide>
-              <img src={banner03} alt="Banner 3" className='w-full h-full'/>
+              <img src={banner03} alt="Banner 3" className='w-full h-full' />
             </SwiperSlide>
           </Swiper>
         </div>
 
-       
         <div>
           <h1 className='text-base font-semibold pt-4 pb-2'>Lanches Recomendados</h1>
           <div className='flex px-1 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
-           {dadosLanches.map((lanche) => (
-             <CardLanche key={lanche.id} 
-             titulo={lanche.titulo} 
-             vendedor={lanche.vendedor} 
-             preco={lanche.preco} 
-             imagem={lanche.imagem}/>
-           ))}
-            
+            {dadosLanches.map((lanche) => (
+              <CardLanche key={lanche.id}
+                titulo={lanche.titulo}
+                vendedor={lanche.vendedor}
+                preco={lanche.preco}
+                imagem={lanche.imagem} />
+            ))}
           </div>
+
           <div className='pt-4'>
             <h1 className='text-base font-semibold pt-4 pb-2'>Vendedores</h1>
             <div className='flex flex-col gap-4'>
-            {dadosVendedor.map((vendedor) => (
-             <CardVendedor
-              key={vendedor.id}
-              nome={vendedor.nome}
-              telefone={vendedor.telefone}
-              status={vendedor.status}
-              imagem={vendedor.imagem}
+              {dadosVendedor.slice(0, vendedoresExibidos).map((vendedor) => (
+                <CardVendedor
+                  key={vendedor.id}
+                  nome={vendedor.nome}
+                  telefone={vendedor.telefone}
+                  status={vendedor.status}
+                  imagem={vendedor.imagem}
                 />
               ))}
             </div>
-           
+
+            {loadingMore && (
+              <div className="flex justify-center mt-4">
+                <Loading />
+              </div>
+            )}
           </div>
         </div>
       </div>
+
       <NavInferior />
     </div>
   );
