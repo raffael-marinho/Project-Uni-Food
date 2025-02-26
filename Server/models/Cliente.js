0c67ab229e7204221c0381d6aab0182589efb775
raffael-marinho <silvamarinho10@gmail.com>
2025-02-26 14:08:25 -0300

    fix:Refatora model Cliente para atender ao formato solicitado

--- Server/models/Cliente.js
+++ Server/models/Cliente.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
-// Esquema do Cliente
 const clienteSchema = new mongoose.Schema({
   nome: {
     type: String,
@@ -22,22 +20,10 @@ const clienteSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  endereco: {
-    rua: String,
-    numero: String,
-    cidade: String,
-    estado: String,
-    cep: String,
-  },
-  imagemPerfil: {
-    type: String, // Caminho ou URL da imagem
-    default: '',
-  },
   pedidos: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Pedido', // Relaciona os pedidos ao cliente
+    ref: 'Pedido',
   }],
 }, { timestamps: true });
-
 
 module.exports = mongoose.model('Cliente', clienteSchema);
