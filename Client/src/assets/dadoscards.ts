a6c66c6949b0afba21f522718992096f83cedb96
pauloJ-r <pj74586@gmail.com>
2025-02-26 16:36:45 -0300

    style: adicionando o campo codigo no arquivo TS

--- Client/src/assets/dadoscards.ts
+++ Client/src/assets/dadoscards.ts
@@ -7,7 +7,8 @@ export const dadosLanches = [
         ingredientes: "Hamburguer de carne bovina, tomate, cebola, alface, maionese",
         preco: "7.50",
         imagem: "https://blog.biglar.com.br/wp-content/uploads/2024/08/iStock-1398630614.jpg",
-        quantidade: 50
+        quantidade: 50,
+        code: "10"
     },
     {
         id: 2,
@@ -17,7 +18,8 @@ export const dadosLanches = [
         vendedor: "Pizzaria da Avenida",
         preco: "29.90",
         imagem: "https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3277542:1663012513/Pizza-de-calabresa.jpg?f=default&$p$f=00df3e9",
-        quantidade: 3
+        quantidade: 3,
+        code: "20"
     },
     {
         id: 3,
@@ -27,6 +29,7 @@ export const dadosLanches = [
         vendedor: "Salgados do João",
         preco: "5.00",
         imagem: "https://cdn.casaeculinaria.com/wp-content/uploads/2023/05/16114400/Massa-de-coxinha.webp",
-        quantidade: 3
+        quantidade: 3,
+        code: "3Y"
     }
 ];
