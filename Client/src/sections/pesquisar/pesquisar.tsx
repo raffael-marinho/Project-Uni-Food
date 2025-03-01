ef33564462b7f668fb97d8c5fb9d8f8890c73e8f
PdrVinicius <pedro.lopesxavier@gmail.com>
2025-02-28 23:06:19 -0300

    feat: criação dos componentes de produtos da pagina de pesquisa

--- Client/src/sections/pesquisar/pesquisar.tsx
+++ Client/src/sections/pesquisar/pesquisar.tsx
@@ -2,24 +2,40 @@ import NavBar from "../../components/Navbar"
 import NavInferior from "../../components/Navinferior"
 import { useNav } from "../../context/nav-context"
 import { useEffect } from "react";
-import { useAuth } from "../../context/auth-context";
+import CardPesquisa from "../../components/CardPesquisa"; 
+import BarraPesque from "../../components/BarraPesque";
 
 const Pesquisar = () => {
     const { setActiveTab } = useNav();
-    const { user } = useAuth();
 
     useEffect(() => {
         setActiveTab("pesquisar");
       }, []);
+
+    const Pesquisar = [
+        { nome: "Hambúrguer", imagem: "https://www.sabornamesa.com.br/media/k2/items/cache/bf1e20a4462b71e3cc4cece2a8c96ac8_XL.jpg", valor: " 7.50", descricao: "Delicioso hambúrguer artesanal" },
+        { nome: "Coxinha", imagem: "https://i0.wp.com/canaldareceita.com.br/wp-content/uploads/2024/09/COXINHA-FIT.jpg?fit=1000%2C600&ssl=1", valor: " 3.50", descricao: "Coxinha de frango com massa crocante" },
+    ];
       
     return (
-        <div className="flex flex-col h-screen">
-            <NavBar/>
-            <div className="flex flex-col items-center mt-28">
-                <h1 className="text-3xl font-bold mb-4">Pesquisar</h1>
-                <p>Bem-vindo, {user?.nome}</p>
-            </div>
-            <NavInferior/>
+       <div className="flex flex-col h-screen">
+            <NavBar />
+            <BarraPesque />
+            <div className="flex flex-col  p-5  text-base font-semibold pb-2">
+                <div className="flex flex-col gap-4">
+
+                    {Pesquisar.map((pesquisa, index) => (
+                        <CardPesquisa 
+                            key={index} 
+                            nome={pesquisa.nome} 
+                            imagem={pesquisa.imagem} 
+                            descricao={pesquisa.descricao}
+                            valor={pesquisa.valor}
+                        />
+                    ))}
+                 </div>
+             </div>
+            <NavInferior />
         </div>
     )
 }
