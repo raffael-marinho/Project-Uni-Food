8908f9d50077b04455b83bba89812ce545484cd2
pauloJ-r <pj74586@gmail.com>
2025-03-02 14:12:34 -0300

    feat: pagina de perfil com icone e mudança de rotas

--- Client/src/sections/perfil/perfil.tsx
+++ Client/src/sections/perfil/perfil.tsx
@@ -5,10 +5,13 @@ import { useNav } from "../../context/nav-context";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/context/auth-context";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { ImgUser } from "@/assets/imagens";
 
 const Perfil = () => {
     const { setActiveTab } = useNav();
     const { user } = useAuth(); 
+    const { logout } = useAuth();
     const navigate = useNavigate(); // Hook para navegação
 
     useEffect(() => {
@@ -18,20 +21,26 @@ const Perfil = () => {
 
     // Função para sair e redirecionar para login
     const handleLogout = () => {
-        localStorage.removeItem("user"); // Remove o usuário do localStorage
-        navigate("/logincliente"); // Redireciona para a tela de login
+        logout ();
+        navigate("/"); 
     };
 
     return (
         <div className="flex flex-col h-screen">
             <NavBar />
 
-            <div className="flex flex-col items-center justify-center h-screen">
-                <h1 className="text-3xl font-bold mb-4">{user?.nome || "Usuário"}</h1>
+            <div className="flex flex-col items-center justify-center h-4/6 w-full">
+                    <Avatar className="w-32 h-32 m-2 flex items-center justify-center mb-9">
+                        <AvatarImage
+                            src={ImgUser}
+                            className="rounded-full w-full h-full object-cover"
+                        />
+                    </Avatar>
+                <h1 className="text-3xl  font-bold mb-5 overflow-hidden text-ellipsis whitespace-nowrap w-3/4 text-center">{user?.nome || "Usuário"}</h1>
                 <div>
                     <Button
                         variant={"link"}
-                        className="text-lg w-60 flex items-center gap-2 font-semibold"
+                        className="text-xl w-60 flex text-center mb-2 font-semibold"
                         onClick={() => navigate(`/editarperfil/${user?.id}`)} 
                     >
                         Editar Perfil
@@ -39,7 +48,7 @@ const Perfil = () => {
 
                     <Button
                         variant={"link"}
-                        className="text-lg w-60 flex items-center gap-2 font-semibold"
+                        className="text-xl w-60 flex text-center gap-7 font-semibold"
                         onClick={handleLogout}
                     >
                         Sair
