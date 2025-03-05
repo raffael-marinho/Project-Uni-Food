31e942aaed194b4d07906cdde32a54898e564b3c
pauloJ-r <pj74586@gmail.com>
2025-03-05 15:43:24 -0300

    feat: finalizando barra de pesquisa com funcionalidade

--- Client/src/components/BarraPesque.tsx
+++ Client/src/components/BarraPesque.tsx
@@ -1,39 +1,29 @@
 import React from "react";
-import { Field, Form, Formik } from "formik";
 import { Search } from 'lucide-react';
 
+interface BarraPesqueProps {
+  termo: string;
+  setTermo: React.Dispatch<React.SetStateAction<string>>;
+}
 
-const BarraPesque: React.FC = () => {
+const BarraPesque: React.FC<BarraPesqueProps> = ({ termo, setTermo }) => {
   return (
-    <div> 
-        <Formik
-        initialValues={{ Pesquisa: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-        >
- <Form className="flex flex-col gap-4 items-center">
-    {/* Form Para Email */}
-    <div className="flex flex-col w-[300px]">
-      <div className="relative">
-        <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#CE9E7E]" />
-        <Field
-          name="Pesquisa"
-          type="Search"
-          placeholder="Pesquisar"
-          className="border-2 border-[#CE9E7E] p-2 pr-10 pl-2 rounded-sm w-[300px] bg-transparent placeholder-[#CE9E7E]
-          text-foreground focus:border-foreground focus:outline-none focus:text-foreground"
-        />
+    <div className="flex flex-col gap-4 items-center">
+      <div className="flex flex-col w-[300px]">
+        <div className="relative">
+          <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#CE9E7E]" />
+          <input
+            type="search"
+            value={termo}
+            onChange={(e) => setTermo(e.target.value)} // Atualiza o termo enquanto digita
+            placeholder="Pesquisar vendedores..."
+            className="border-2 border-[#CE9E7E] p-2 pr-10 pl-2 rounded-sm w-[300px] bg-transparent placeholder-[#CE9E7E]
+              text-foreground focus:border-foreground focus:outline-none focus:text-foreground"
+          />
+        </div>
       </div>
-      </div>
-    </Form>
-    </Formik>
     </div>
-   
-    
-  )
-    
-
+  );
 };
 
 export default BarraPesque;
