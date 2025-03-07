cb2c3ee1f300ccbfe87b4ab05ebafdf7ae3dc626
pauloJ-r <pj74586@gmail.com>
2025-03-07 02:29:48 -0300

    fix: ajustando nome do card de produto em pesquisa

--- Client/src/components/CardPesquisa.tsx
+++ Client/src/components/CardPesquisa.tsx
@@ -6,14 +6,14 @@ interface CardPesquisaProps {
   valor: string;
 }
 
-const CardPedido: React.FC<CardPesquisaProps> = ({ nome, descricao, imagem, valor }) => {
+const CardProdutoPesq: React.FC<CardPesquisaProps> = ({ nome, descricao, imagem, valor }) => {
   return (
     <div className="w-full h-[108px] flex-shrink-0 bg-[#FFF6E0] rounded-lg flex items-center shadow-md overflow-hidden">
       
       {/* Imagem do produto no lado esquerdo */}
       <div className="w-24 h-full flex-shrink-0">
         <img
-          src={imagem || "https://ui.shadcn.com/avatars/04.png"}
+          src={imagem }
           alt={nome}
           className="w-full h-full object-cover rounded-l-lg"
         />
@@ -29,13 +29,13 @@ const CardPedido: React.FC<CardPesquisaProps> = ({ nome, descricao, imagem, valo
         </p>
        
       </div>
-      <div className="w-10/12 mt-4 pb-2">
-                <p className="text-lg font-semibold text-center"> R$ 
-                    {valor}
-                    </p>
-            </div>
+      <div className="mt-4 pb-2 w-16"> {/* Ajuste o valor conforme necessário */}
+  <p className="text-lg font-semibold text-tertiary mt-1 overflow-hidden text-ellipsis whitespace-nowrap">
+    R$&nbsp;{valor}
+  </p>
+</div>
     </div>
   );
 };
 
-export default CardPedido;
+export default CardProdutoPesq;
