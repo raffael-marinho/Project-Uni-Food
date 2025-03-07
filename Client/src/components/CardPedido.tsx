eba10edf0e3b59f030142b27c716a70e5c71bedf
pauloJ-r <pj74586@gmail.com>
2025-03-07 01:17:59 -0300

    feat: adicionando status de em andamento

--- Client/src/components/CardPedido.tsx
+++ Client/src/components/CardPedido.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle } from "lucide-react";
+import { CheckCircle, XCircle } from "lucide-react";
 import DivisaoReserva from "@/assets/imagens/DivisãoReserva.svg";
 
 interface CardPedidoProps {
@@ -6,17 +6,18 @@ interface CardPedidoProps {
   descricao: string;
   imagem: string;
   code: string;
-  confirmado: boolean;
+  status: string;  // Alterando para somente usar o status como parâmetro
+
 }
 
-const CardPedido: React.FC<CardPedidoProps> = ({ nome, descricao, imagem, code, confirmado }) => {
+const CardPedido: React.FC<CardPedidoProps> = ({ nome, descricao, imagem, code, status }) => {
   return (
     <div className="w-full h-[108px] flex-shrink-0 bg-[#FFF6E0] rounded-lg flex items-center shadow-md overflow-hidden">
       
       {/* Imagem do produto no lado esquerdo */}
       <div className="w-24 h-full flex-shrink-0">
         <img
-          src={imagem || "https://ui.shadcn.com/avatars/04.png"}
+          src={imagem }
           alt={nome}
           className="w-full h-full object-cover rounded-l-lg"
         />
@@ -39,12 +40,16 @@ const CardPedido: React.FC<CardPedidoProps> = ({ nome, descricao, imagem, code, 
 
       {/* Ícone de confirmação OU código */}
       <div className="w-24 h-full flex items-center justify-center bg-[#FFF6E0]">
-        {confirmado ? (
+        {status === "Cancelado" ? (
+          <div className="flex flex-col items-center justify-center h-full">
+            <XCircle className="text-red-600 w-8 h-8" />
+          </div>
+        ) : status === "Finalizado" ? (
           <CheckCircle className="text-green-600 w-8 h-8" />
         ) : (
           <div className="flex flex-col items-center justify-center h-full">
-            <span className="text-xs font-semibold text-[#FF6A07]">code</span>
-            <p className="text-xl font-bold text-[#FF6A07]">{code}</p>
+            <span className="text-sm font-semibold text-primary">Code</span>
+            <p className="text-xl font-bold text-primary">{code}</p>
           </div>
         )}
       </div>
