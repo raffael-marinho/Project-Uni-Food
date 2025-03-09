4d6e6996206735b17512fbde7ad8eb033bf17f94
pauloJ-r <pj74586@gmail.com>
2025-03-09 04:26:39 -0300

    feat: adicionando pagina de pedidos

--- Client/src/sections/pedidos/pedidos.tsx
+++ Client/src/sections/pedidos/pedidos.tsx
@@ -7,6 +7,8 @@ import CardPedido from "../../components/CardPedido";
 import { useAuth } from "../../context/auth-context";
 import apiUrl from "@/utils/Api";
 import Loading from "@/components/Loading";
+import { XIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 interface Produto {
     nome: string;
@@ -18,32 +20,37 @@ interface PedidoProduto {
     produto: Produto;
     quantidade: number;
     precoUnitario: number;
+
 }
 
 interface Pedido {
-    code: string;
+    codigoPedido: string;
     status: string;
     produtos: PedidoProduto[];
+    dataPedido: string;
+    chavePix: string;
 }
 
 const Pedidos = () => {
     const { setActiveTab } = useNav();
-    const { user } = useAuth();  // Obtendo o usuário logado
+    const { user } = useAuth();  
     const [pedidos, setPedidos] = useState<Pedido[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
+    const [selectedPedido, setSelectedPedido] = useState<Pedido | null>(null);
+    const [modalOpen, setModalOpen] = useState(false);
+
 
     useEffect(() => {
         setActiveTab("pedidos");
     }, [setActiveTab]);
 
     useEffect(() => {
-        if (user && user.id) {  // Agora usando user.id como o clienteId
+        if (user && user.id) {  
             const fetchPedidos = async () => {
                 try {
                     const response = await axios.get(`${apiUrl}/pedidos/cliente/${user.id}`);
-                    console.log(response.data); // Verifique os dados retornados pela API
-                    setPedidos(response.data);  // Atualizando apenas 'pedidos'
+                    setPedidos(response.data);
                     setLoading(false);
                 } catch (err) {
                     setError("Erro ao carregar os pedidos.");
@@ -55,18 +62,28 @@ const Pedidos = () => {
         }
     }, [user]);
 
+    const openModal = (pedido: Pedido) => {
+        setSelectedPedido(pedido);
+        setModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedPedido(null);
+        setModalOpen(false);
+    };
+
     return (
         <div className="flex flex-col h-screen">
             <NavBar />
             <div>
                 {loading && (
                     <div className="flex justify-center items-center h-screen pb-64">
-                        <Loading />  {/* Aqui você chama o componente Loading */}
+                        <Loading /> 
                     </div>
                 )}
                 {error && !loading && (
                     <div className="flex justify-center items-center h-screen pb-64">
-                        <p className="text-primary text-lg">{error}</p>  {/* Exibe a mensagem de erro em vermelho */}
+                        <p className="text-primary text-lg">{error}</p>
                     </div>
                 )}
             </div>
@@ -78,24 +95,86 @@ const Pedidos = () => {
                         {pedidos.length === 0 && !loading ? (
                             <p className="text-lg flex items-center">Você não tem pedidos.</p>
                         ) : (
-                            pedidos.map((pedido, index) => (
-                                pedido.produtos && pedido.produtos.length > 0 ? (
-                                    pedido.produtos.map((produto, produtoIndex) => (
-                                        <CardPedido
-                                            key={`${index}-${produtoIndex}`}
-                                            nome={produto.produto.nome}  // Acessando nome do produto
-                                            imagem={produto.produto.imagem}  // Acessando imagem do produto
-                                            descricao={produto.produto.descricao}  // Acessando descrição do produto
-                                            code={pedido.code}  // Usando o ID do pedido como código
-                                            status={pedido.status}
-                                        />
-                                    ))
-                                ) : (
-                                    <p>Este pedido não contém produtos.</p>
-                                )
-                            ))
+                            pedidos
+                                .slice() 
+                                .sort((a, b) => new Date(b.dataPedido).getTime() - new Date(a.dataPedido).getTime()) // Ordena do mais recente para o mais antigo
+                                .map((pedido, index) => (
+                                    pedido.produtos && pedido.produtos.length > 0 ? (
+                                        pedido.produtos.map((produto, produtoIndex) => (
+                                            <CardPedido
+                                                key={`${index}-${produtoIndex}`}
+                                                nome={produto.produto.nome}
+                                                imagem={produto.produto.imagem}
+                                                descricao={produto.produto.descricao}
+                                                code={pedido.codigoPedido}
+                                                status={pedido.status}
+                                                onClick={() => void openModal(pedido)}
+                                            />
+                                        ))
+                                    ) : (
+                                        <p>Este pedido não contém produtos.</p>
+                                    )
+                                ))
                         )}
                     </div>
+                    <div>
+
+                    </div>
+                    {modalOpen && selectedPedido && (
+                        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 p-4">
+                            <div className="bg-[#FFF6E0] p-6 rounded-lg shadow-lg w-96 relative">
+
+                                <button
+                                    onClick={closeModal}
+                                    className="absolute top-2 right-2 text-primary p-3 rounded-full hover:bg-primary hover:text-white duration-200"
+                                >
+                                    <XIcon className="h-6 w-6" />
+                                </button>
+
+                                <h2 className="text-xl font-semibold mb-4">Código {selectedPedido.codigoPedido}</h2>
+                                <p className="text-sm text-primary mb-4">Status: {selectedPedido.status}</p>
+
+                                <h3 className="font-semibold mb-2">Produtos:</h3>
+                                <ul>
+                                    {selectedPedido.produtos.map((produto, index) => (
+                                        <li key={index} className="mb-2">
+                                            <img
+                                                src={produto.produto.imagem}
+                                                alt={produto.produto.nome}
+                                                className="w-16 h-16 rounded-md mr-2 inline-block"
+                                            />
+                                            <div className="inline-block align-middle">
+                                                <p className="font-semibold">{produto.produto.nome}</p>
+                                                <p className="text-sm">Quantidade: {produto.quantidade}</p>
+                                                <p className="text-sm">Preço: R$ {produto.precoUnitario}</p>
+                                                <p className="text-sm pb-2">Total: R$ {produto.precoUnitario * produto.quantidade}</p>
+                                            
+                                                {selectedPedido.status !== "Finalizado" && selectedPedido.status !== "Cancelado" ? (
+                                                    <p className="text-sm text-ellipsis overflow-hidden whitespace-nowrap w-44">
+                                                        Chave Pix: {selectedPedido.chavePix}
+                                                    </p>
+                                                ) : (
+                                                    <p className="text-sm text-primary">
+                                                        {selectedPedido.status === "Finalizado" ? "Produto Pago" : "Produto Cancelado"}
+                                                    </p>
+                                                )}
+
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                                {selectedPedido.status !== "Finalizado" && selectedPedido.status !== "Cancelado" && (
+                                    <Button
+                                        onClick={() => navigator.clipboard.writeText(selectedPedido.chavePix)}
+                                        className="mt-4 w-full p-2 rounded-lg"
+                                    >
+                                        Copiar Chave Pix
+                                    </Button>
+                                )}
+                            </div>
+                        </div>
+                    )}
+
                 </div>
             </div>
             <NavInferior />
