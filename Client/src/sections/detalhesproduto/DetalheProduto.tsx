487a8f39541d975b541e7fd0a379428cfae9dd50
pauloJ-r <pj74586@gmail.com>
2025-03-09 06:09:11 -0300

    feat: adicionando o handleComprar

--- Client/src/sections/detalhesproduto/DetalheProduto.tsx
+++ Client/src/sections/detalhesproduto/DetalheProduto.tsx
@@ -6,12 +6,16 @@ import Loading from "@/components/Loading";
 import { useNav } from "@/context/nav-context";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
+import { useAuth } from "@/context/auth-context";
 import apiUrl from "@/utils/Api";
 
 interface DetailLanchesProps {
   _id: string;
   nome: string;
-  vendedor: { nome: string };
+  vendedor: {
+    _id: string;
+    nome: string;
+  };
   preco: number;
   imagem: string;
   descricao: string;
@@ -19,47 +23,93 @@ interface DetailLanchesProps {
   quantidade: number;
 }
 
+
+
 const DetalhesProduto: React.FC = () => {
   const { id } = useParams();
-  const { pathname } = useLocation();
+  const { token } = useAuth();
+  const { user } = useAuth();
+  const { pathname, search } = useLocation();
   const navigate = useNavigate();
-  const { search } = useLocation();
+  const { setActiveTab } = useNav();
   const queryParams = new URLSearchParams(search);
   const tab = queryParams.get("tab");
-  const { setActiveTab } = useNav();
   const [lanche, setLanche] = useState<DetailLanchesProps | null>(null);
   const [loading, setLoading] = useState(true);
   const [quantidade, setQuantidade] = useState(0);
   const [limiteMaximo, setLimiteMaximo] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
   const total = quantidade * (lanche?.preco || 0);
 
   useEffect(() => {
-      if (tab === "pesquisar") {
-        setActiveTab("pesquisar");
-      } else {
-        setActiveTab("inicio");
-      }
+    setActiveTab(tab === "pesquisar" ? "pesquisar" : "inicio");
+
     setLoading(true);
-    
     fetch(`${apiUrl}/produto/${id}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) throw new Error("Erro ao buscar produto");
+        return res.json();
+      })
       .then((data) => {
         setLanche(data);
         setLimiteMaximo(data.quantidade);
       })
-      .catch((err) => console.error("Erro ao buscar produto:", err))
+      .catch((err) => console.error(err))
       .finally(() => setLoading(false));
   }, [id, pathname, setActiveTab]);
 
   const aumentarQuantidade = () => {
-    if (quantidade < limiteMaximo) {
-      setQuantidade(quantidade + 1);
-    }
+    if (quantidade < limiteMaximo) setQuantidade((prev) => prev + 1);
   };
 
   const diminuirQuantidade = () => {
-    if (quantidade > 0) {
-      setQuantidade(quantidade - 1);
+    if (quantidade > 0) setQuantidade((prev) => prev - 1);
+  };
+
+  const handleComprar = async () => {
+    if (quantidade === 0 || !lanche?._id) return;
+  
+    setIsLoading(true);
+  
+    try {
+      if (!token || !user) {
+        console.error("Usuário não autenticado");
+        navigate("/erropedido");
+        return;
+      }
+  
+      const clienteId = user?.id; 
+      const vendedorId = lanche.vendedor._id;  
+      const response = await fetch(`${apiUrl}/pedidos`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          clienteId,
+          vendedorId,
+          produtos: [
+            {
+              produtoId: lanche._id,
+              quantidade,
+            },
+          ],
+        }),
+      });
+  
+      if (response.ok) {
+        navigate("/confirmarpedido");
+      } else {
+        console.error("Erro na compra:", await response.text());
+        navigate("/erropedido");
+      }
+    } catch (error) {
+      console.error("Erro ao finalizar compra:", error);
+      navigate("/error");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -91,18 +141,17 @@ const DetalhesProduto: React.FC = () => {
     <div className="flex flex-col h-screen">
       <Navbar />
       <div className="relative">
-      <button
-          onClick={() => navigate(-1)} 
-          className="absolute top-4 left-4 bg-primary p-2 rounded-full shadow-md"
-        >
+        <button onClick={() => navigate(-1)} className="absolute top-4 left-4 bg-primary p-2 rounded-full shadow-md">
           <ArrowLeft className="w-6 h-6 text-white" />
         </button>
         <img src={lanche.imagem} className="h-44 w-full object-cover" alt={lanche.nome} />
       </div>
+
       <div className="flex flex-row items-center w-full pl-4 mt-2 justify-between">
         <h1 className="text-2xl font-bold w-3/4 overflow-hidden text-ellipsis whitespace-nowrap">{lanche.nome}</h1>
         <p className="text-xl font-semibold pl-1 text-primary w-1/3">R$ {lanche.preco.toFixed(2)}</p>
       </div>
+
       <div className="flex flex-col items-start w-full pl-4">
         <p className="text-sm w-3/4 overflow-hidden text-ellipsis">{lanche.descricao}</p>
         <h1 className="text-lg font-semibold mt-8">Ingredientes</h1>
@@ -116,8 +165,11 @@ const DetalhesProduto: React.FC = () => {
         <button onClick={aumentarQuantidade} className="bg-[#FAAB35] p-2 rounded-full w-5 h-5 flex items-center justify-center text-lg font-bold text-foreground">+</button>
         <h1 className="text-lg font-semibold ml-4">R$ {total.toFixed(2)}</h1>
       </div>
+
       <div className="flex flex-col items-center">
-        <Button className="w-80 flex items-center gap-2 mt-8" onClick={() => {} } disabled={quantidade === 0}>Comprar</Button>
+        <Button className="w-80 flex items-center gap-2 mt-8" onClick={handleComprar} disabled={quantidade === 0 || isLoading}>
+          {isLoading ? "Processando..." : "Comprar"}
+        </Button>
       </div>
 
       <NavInferior />
