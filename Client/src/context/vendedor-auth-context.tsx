de3d98d604be4878363824eebfb8cc02e06c64d8
pauloJ-r <pj74586@gmail.com>
2025-03-10 17:54:20 -0300

    feat: criando context de vendedor

---
+++ Client/src/context/vendedor-auth-context.tsx
@@ -0,0 +1,80 @@
+import React, { createContext, useState, useContext, ReactNode, useEffect } from 'react';
+
+interface Vendedor {
+  id: string;
+  nome: string;
+  email: string;
+  telefone: string;
+  descricao: string;
+  imagemPerfil?: string;
+  status: 'Aberto' | 'Fechado';
+  chavePix?: string;
+  imagemCapa?: string;
+}
+
+interface VendedorContextProps {
+  vendedor: Vendedor | null;
+  loading: boolean;
+  login: (vendedor: Vendedor, token: string) => void;
+  logout: () => void;
+  updateVendedor: (updatedVendedor: Partial<Vendedor>) => void;
+  token: string | null;
+}
+
+const VendedorContext = createContext<VendedorContextProps>({} as VendedorContextProps);
+
+export const useVendedor = () => {
+  const context = useContext(VendedorContext);
+  if (!context) {
+    throw new Error("useVendedor deve ser usado dentro de um VendedorProvider");
+  }
+  return context;
+};
+
+export const VendedorProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [vendedor, setVendedorState] = useState<Vendedor | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const storedVendedor = localStorage.getItem("vendedor");
+    const storedToken = localStorage.getItem("token");
+
+    if (storedVendedor && storedToken) {
+      setVendedorState(JSON.parse(storedVendedor));
+      setToken(storedToken);
+    }
+    setLoading(false);
+  }, []);
+
+  const login = (vendedor: Vendedor, token: string) => {
+    setVendedorState(vendedor);
+    setToken(token);
+    localStorage.setItem("vendedor", JSON.stringify(vendedor));
+    localStorage.setItem("token", token);
+  };
+
+  const logout = () => {
+    setVendedorState(null);
+    setToken(null);
+    localStorage.removeItem("vendedor");
+    localStorage.removeItem("token");
+  };
+
+  const updateVendedor = (updatedVendedor: Partial<Vendedor>) => {
+    setVendedorState((prevVendedor) => {
+      if (prevVendedor) {
+        const updated = { ...prevVendedor, ...updatedVendedor };
+        localStorage.setItem("vendedor", JSON.stringify(updated));
+        return updated;
+      }
+      return prevVendedor;
+    });
+  };
+
+  return (
+    <VendedorContext.Provider value={{ vendedor, token, loading, login, logout, updateVendedor }}>
+      {children}
+    </VendedorContext.Provider>
+  );
+};
