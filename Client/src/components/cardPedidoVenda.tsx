34368f68b9a6382980b27abbc802036b1b87e886
pauloJ-r <pj74586@gmail.com>
2025-03-12 01:53:56 -0300

    feat: cardpedido venda atualizado

--- Client/src/components/cardPedidoVenda.tsx
+++ Client/src/components/cardPedidoVenda.tsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
 import { CheckCircle } from "lucide-react";
-import DivisaoReserva from "@/assets/imagens/DivisãoReserva.svg";
-import PedidoCard from "../components/CardDetalhePedido";
+import DivisaoReserva from "../assets/imagens/DivisãoReserva.svg";
+
+
+interface Produto {
+  nome: string;
+  precoUnitario: number;
+  quantidade: number;
+}
 
 interface CardPedidoVendasProps {
   nome: string;
-  code: string;
-  confirmado: boolean;
-  pedido: { itens: { nome: string; preco: number }[]; total: number };
+  codigoPedido: string;
+  status: string;
+  produtos: Produto[];
+  total: number;
+  onOpen: () => void; // Função para abrir o modal
+  onClose: () => void; // Função para fechar o modal
+  onClick: () => void;
 }
 
-const CardPedidoVendas: React.FC<CardPedidoVendasProps> = ({ nome, code, confirmado, pedido }) => {
-  const [modalAberto, setModalAberto] = useState(false);
+const CardPedidoVendas: React.FC<CardPedidoVendasProps> = ({
+  nome,
+  codigoPedido,
+  status,
+  onOpen, // Função para abrir o modal
+}) => {
 
   return (
     <>
       <div
         className="w-full h-[108px] flex-shrink-0 bg-[#FFF6E0] rounded-lg flex items-center shadow-md overflow-hidden border border-[#E5C690] cursor-pointer"
-        onClick={() => setModalAberto(true)}
+        onClick={onOpen} // Abre o modal quando o pedido é clicado
       >
         {/* Nome do Cliente */}
         <div className="flex-1 px-4 text-brown-800 font-semibold text-lg">{nome}</div>
@@ -29,26 +42,16 @@ const CardPedidoVendas: React.FC<CardPedidoVendasProps> = ({ nome, code, confirm
 
         {/* Código do Pedido ou Confirmação */}
         <div className="w-24 flex flex-col items-center justify-center h-full bg-[#FFF6E0]">
-          {confirmado ? (
+          {status === "Finalizado" ? (
             <CheckCircle className="text-green-600 w-8 h-8" />
           ) : (
             <>
               <span className="text-xs font-semibold text-[#FF6A07]">code</span>
-              <p className="text-xl font-bold text-[#FF6A07]">{code}</p>
+              <p className="text-xl font-bold text-[#FF6A07]">{codigoPedido}</p>
             </>
           )}
         </div>
       </div>
-
-      {/* Modal de Detalhes do Pedido */}
-      {modalAberto && (
-        <PedidoCard
-          pedido={pedido}
-          cliente={nome}
-          codigo={code}
-          onClose={() => setModalAberto(false)}
-        />
-      )}
     </>
   );
 };
