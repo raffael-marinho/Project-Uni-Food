b184cbaaf7f30b0d0d359f2e6d33c68fafdc1412
pauloJ-r <pj74586@gmail.com>
2025-03-19 16:27:48 -0300

    fix: ajustando responsividade nas telas

--- Client/src/sections/perfil/editarperfil.tsx
+++ Client/src/sections/perfil/editarperfil.tsx
@@ -1,6 +1,6 @@
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { useAuth } from "../../context/auth-context"; 
+import { useAuth } from "../../context/auth-context";
 import axios from "axios";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
@@ -17,8 +17,8 @@ interface User {
 }
 
 const EditarPerfil: React.FC = () => {
-  const { id } = useParams<{ id: string }>(); 
-  const { user, updateUser} = useAuth();
+  const { id } = useParams<{ id: string }>();
+  const { user, updateUser } = useAuth();
   const navigate = useNavigate();
   const { toast } = useToast();
   const [userData, setUserData] = useState<User | null>(null);
@@ -29,7 +29,7 @@ const EditarPerfil: React.FC = () => {
     nome: Yup.string()
       .min(3, "O nome deve ter pelo menos 3 caracteres")
       .max(50, "O nome não pode ter mais de 50 caracteres")
-      .required("O nome é obrigatório"), 
+      .required("O nome é obrigatório"),
   });
 
   useEffect(() => {
@@ -37,13 +37,13 @@ const EditarPerfil: React.FC = () => {
       console.warn("Token ou ID do usuário ainda não carregado, esperando...");
       return;
     }
-  
+
     const fetchUserData = async () => {
       try {
         const { data } = await axios.get<User>(`${apiUrl}/cliente/${id}`, {
           headers: { Authorization: `Bearer ${token}` },
         });
-  
+
         setUserData(data);
       } catch (error) {
         console.error("Erro ao carregar usuário:", error);
@@ -57,7 +57,7 @@ const EditarPerfil: React.FC = () => {
         setLoading(false);
       }
     };
-  
+
     fetchUserData();
   }, [token, id]);
 
@@ -68,14 +68,14 @@ const EditarPerfil: React.FC = () => {
       if (values.nome !== userData?.nome) {
         updatedFields.nome = values.nome;
       }
-  
-       if (values.telefone && values.telefone !== userData?.telefone) {
- 
+
+      if (values.telefone && values.telefone !== userData?.telefone) {
+
         updatedFields.telefone = `+5581${values.telefone.replace(/[^0-9]/g, "")}`;
       } else if (!values.telefone) {
-        updatedFields.telefone = null; 
+        updatedFields.telefone = null;
       }
-  
+
       if (Object.keys(updatedFields).length === 0) {
         toast({
           title: "Nenhuma alteração",
@@ -86,13 +86,13 @@ const EditarPerfil: React.FC = () => {
         });
         return;
       }
-  
+
       await axios.put(
         `${apiUrl}/cliente/${id}`,
         updatedFields,
         { headers: { Authorization: `Bearer ${token}` } }
       );
-  
+
       toast({
         title: "Perfil atualizado",
         description: "Seu perfil foi atualizado com sucesso.",
@@ -100,12 +100,12 @@ const EditarPerfil: React.FC = () => {
         duration: 2000,
         className: "translate-y-7",
       });
-  
-  
+
+
       if (user) {
         updateUser({ ...user, ...updatedFields });
       }
-  
+
       navigate(`/perfil/${id}`);
     } catch (error) {
       console.error("Erro ao atualizar o perfil:", error);
@@ -117,66 +117,69 @@ const EditarPerfil: React.FC = () => {
         className: "translate-y-7",
       });
     }
-    
+
   };
-  
-  
+
+
   return (
-    <div className="flex flex-col h-screen">
-      
+    <div className="flex flex-col h-screen overflow-x-hidden">
+
       <div className="flex p-6">
         <Link to={`/perfil/${id}`} className="bg-primary p-2 rounded-full shadow-md">
           <ArrowLeft className="w-6 h-6 text-white" />
         </Link>
-        
+
       </div>
-      
+
 
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-3xl font-bold mb-5">Editar Perfil</h1>
         {loading ? (
-          <Loading /> 
+          <Loading />
         ) : (
-        userData && (
-          <Formik
-            initialValues={{ nome: userData.nome, telefone: userData.telefone }} 
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-            enableReinitialize
-          >
-            {({ isSubmitting, setFieldValue }) => (
-              <Form className="flex flex-col">
-                <label htmlFor="nome" className="block mb-2 font-semibold">Nome</label>
-                <Field id="nome" name="nome"
-                  className="border-2 border-[#CE9E7E] p-2 pr-10 pl-2 rounded-sm w-[300px] bg-transparent placeholder-[#CE9E7E]
+          userData && (
+            <Formik
+              initialValues={{ nome: userData.nome, telefone: userData.telefone }}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+              enableReinitialize
+            >
+              {({ isSubmitting, setFieldValue }) => (
+                <Form className="flex flex-col">
+                  <label htmlFor="nome" className="block mb-2 font-semibold">Nome</label>
+                  <Field id="nome" name="nome"
+                    className="border-2 border-[#CE9E7E] p-2 pr-10 pl-2 rounded-sm w-[300px] bg-transparent placeholder-[#CE9E7E]
                   text-foreground focus:border-foreground focus:outline-none focus:text-foreground mb-6"
-                />
-                <ErrorMessage name="nome" component="div" className="text-red-500 text-sm" />
-
-                <label htmlFor="telefone" className="block mb-2 font-semibold">Whatsapp</label>
-                <div className="flex items-center border-2 border-[#CE9E7E] p-2 rounded-sm w-[300px] bg-transparent text-foreground focus-within:border-foreground focus-within:outline-none">
-                  <span className="text-primary">+55 81</span>
-                  <Field
-                    id="telefone"
-                    name="telefone"
-                    type="tel"
-                    placeholder="999999999"
-                    maxLength={9}
-                    className="ml-2 flex-1 bg-transparent outline-none placeholder-[#CE9E7E] text-foreground"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                      let telefone = e.target.value.replace(/[^0-9]/g, "");
-                      if (telefone.length > 9) {
-                        telefone = telefone.slice(0, 9);
-                      }
-                      setFieldValue("telefone", telefone);
-                    }}
                   />
-                </div>
-                <ErrorMessage name="telefone" component="div" className="text-red-500 text-sm" />
+                  <ErrorMessage name="nome" component="div" className="text-primary text-sm" />
 
-                <Button type="submit" disabled={isSubmitting} className="mt-6">
-                  {isSubmitting ? "Salvando..." : "Salvar"}
-                </Button>
+                  <label htmlFor="telefone" className="block mb-2 font-semibold">Whatsapp</label>
+                  <div className="flex items-center border-2 border-[#CE9E7E] p-2 rounded-sm w-[300px] bg-transparent text-foreground focus-within:border-foreground focus-within:outline-none">
+                    <div className="flex flex-row items-center">
+                      <span className="text-primary mr-2">+55</span>
+                      <span className="text-primary mr-2">81</span>{/* Tamanho fixo e margem */}
+                    </div>
+                    <Field
+                      id="telefone"
+                      name="telefone"
+                      type="tel"
+                      placeholder="999999999"
+                      maxLength={9}
+                      className="ml-2 flex-1 bg-transparent outline-none placeholder-[#CE9E7E] text-foreground"
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        let telefone = e.target.value.replace(/[^0-9]/g, "");
+                        if (telefone.length > 9) {
+                          telefone = telefone.slice(0, 9);
+                        }
+                        setFieldValue("telefone", telefone);
+                      }}
+                    />
+                  </div>
+                  <ErrorMessage name="telefone" component="div" className="text-primary text-sm" />
+
+                  <Button type="submit" disabled={isSubmitting} className="mt-6">
+                    {isSubmitting ? "Salvando..." : "Salvar"}
+                  </Button>
                 </Form>
               )}
             </Formik>
